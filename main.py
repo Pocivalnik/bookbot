3fb619e1224921a598c79a1d43274e39da4e6ee4
Pocivalnik <pocivalnikrok@gmail.com>
2025-11-05 10:04:10 +0100

    Created a stats.py to store my functions

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def get_book_text(file_path):
-    with open(file_path) as f:
-        return f.read()
-    
-
-def word_count(text_from_book):
-    return len(text_from_book.split())
-
-
+from stats import get_book_text
+from stats import word_count
 
 def main():
     
